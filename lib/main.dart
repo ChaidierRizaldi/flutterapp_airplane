8b15ed7ce59b10f6015ec3799c70a7c44eb81a66
aldirzld <aldirizaldi318@gmail.com>
2022-01-09 23:27:37 +0700

    Adding Firebase iOS & Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutterapp_airplane/cubit/page_cubit.dart';
@@ -7,8 +8,11 @@ import 'package:flutterapp_airplane/ui/pages/main_page.dart';
 import 'package:flutterapp_airplane/ui/pages/sign_up_page.dart';
 import 'package:flutterapp_airplane/ui/pages/splash_page.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
